2afdf3861d2527be4d6962a36b37871b52dda42f
Alasdair Allen <alasdair.allen@activfinancial.com>
2020-12-13 13:53:43 +0000

    Added 2020/13

---
+++ 2020/13/aoc2020_13b.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+int main(int argc, const char* argv[])
+{
+	scanf_s("%*d\n");
+
+	__int64 a = 0, x;
+	
+	int n = 0;
+	char bus[32];
+	while (!feof(stdin) && scanf_s("%[^,],", bus, (unsigned)_countof(bus)) != 0)
+	{
+		if (strcmp(bus, "x") != 0)
+		{
+			int bus_id;
+			sscanf_s(bus, "%d", &bus_id);
+
+			if (n == 0)
+			{
+				x = bus_id;
+			}
+			else
+			{
+				while ((a + n) % bus_id != 0)
+					a += x;
+
+				x *= bus_id;
+			}
+		}
+
+		n++;
+	}
+
+	printf("%lld", a);
+
+	return 0;
+}
+
+// 93505395
+// 530015546283687
