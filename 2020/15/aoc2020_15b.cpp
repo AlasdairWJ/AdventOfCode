57eef9b7531490c7a152eb0669f0ca461d148536
Alasdair Allen <alasdair.allen@activfinancial.com>
2020-12-15 13:50:35 +0000

    Added 2020/15

---
+++ 2020/15/aoc2020_15b.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <map>
+
+#define N 30000000
+
+int main(int argc, char const *argv[])
+{
+	std::map<int, int> last_indices;
+
+	int n = 0;
+
+	int value;
+	while (scanf_s("%d,", &value) == 1)
+		last_indices[value] = n++;
+
+	for (; n < N; n++)
+	{
+		auto it = last_indices.find(value);
+		if (it == last_indices.end())
+		{
+			last_indices[value] = n - 1;
+			value = 0;
+		}
+		else
+		{
+			value = n - it->second - 1;
+			it->second = n - 1;
+		}
+	}
+	
+	printf("%d", value);
+
+	return 0;
+}
