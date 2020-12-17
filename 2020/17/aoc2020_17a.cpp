aa5f82eee542bb719164dcd47fd2f0cab1d2cd5b
Alasdair Allen <alasdair.allen@activfinancial.com>
2020-12-17 11:10:13 +0000

    Added 2020/17

---
+++ 2020/17/aoc2020_17a.cpp
@@ -0,0 +1,105 @@
+#include <cstdio>
+#include <utility>
+
+constexpr int POW(int base, int exponent)
+{
+	return exponent == 0 ? 1 : base * POW(base, exponent - 1);
+}
+
+const int N = 22;
+const int D = 3;
+const int ND = POW(N, D);
+
+const int I = 8;
+
+using board_type = char[ND];
+
+char& get(board_type& board, int (&pos)[D])
+{
+	int x = 0;
+	for (int d = 0, p = 1; d < D; d++, p *= N)
+		x += p * pos[d];
+	return board[x];
+} 
+
+void update(board_type& current, board_type& next)
+{
+	int pos[D], npos[D];
+	for (int i = 0; i < ND; i++)
+	{
+		const char& current_cell = current[i];
+		char& next_cell = next[i];
+
+		for (int d = 0, x = i; d < D; d++, x /= N)
+			pos[d] = x % N;
+
+		int neighbour_count = 0;
+
+		for (int j = 0; j < POW(3, D); j++)
+		{
+			if (j == (POW(3, D) - 1) / 2)
+				continue; // middle
+
+			bool on_board = true;
+			for (int d = 0, x = j; d < D; d++, x /= 3)
+			{
+				int& p = (npos[d] = pos[d] + (x % 3) - 1);
+				if (p < 0 || p >= N)
+				{
+					on_board = false;
+					break;
+				}
+			}
+
+			if (!on_board)
+				continue;
+
+			if (get(current, npos) == '#')
+				neighbour_count++;
+		}
+
+		if (current_cell == '#')
+		{
+			next_cell = neighbour_count == 2 || neighbour_count == 3 ? '#' : ' ';
+		}
+		else
+		{
+			next_cell = neighbour_count == 3 ? '#' : ' ';
+		}
+	}
+}
+
+board_type buf1 = {}, buf2;
+
+int main(int argc, const char* argv[])
+{
+	board_type *current = &buf1, *next = &buf2;
+
+	int pos[D] = { N/2, 0, 0 };
+	for (int i = 0; i < I; i++)
+	{
+		pos[1] = N/2 - I/2 + i;
+		for (int j = 0; j < I; j++)
+		{
+			pos[2] = N/2 - I/2 + j;
+			get(*current, pos) = getchar();
+		}
+		getchar(); // nl
+	}
+
+	for (int it = 0; it < 6; it++)
+	{
+		update(*current, *next);
+		std::swap(current, next);
+	}
+
+	int count = 0;
+
+	for (int i = 0; i < ND; i++)
+		if ((*current)[i] == '#')
+			count++;
+
+	printf("%d", count);
+
+	return 0;
+}
