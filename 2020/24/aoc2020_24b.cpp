f66d80ba8ace791cf0d3d133ef87e688f1410a62
Alasdair <alasdairwjallen@gmail.com>
2021-12-25 14:31:19 +0000

    Refactored 2020/24

--- 2020/24/aoc2020_24b.cpp
+++ 2020/24/aoc2020_24b.cpp
@@ -1,41 +1,13 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <algorithm> // std::count_if
 
 //   nw ne
 //    |/
 // w--O--e
 //   /|
 // sw se
-
-bool read_direction(const std::string& line, int& position, int& dx, int& dy)
-{
-	switch (line[position++])
-	{
-	case 'e': dx =  1, dy = 0; break;
-	case 'w': dx = -1, dy = 0; break;
-	case 'n':
-		dy = -1;
-		switch (line[position++])
-		{
-		case 'e': dx = 1; break;
-		case 'w': dx = 0; break;
-		default: return false;
-		}
-		break;
-	case 's':
-		dy = 1;
-		switch (line[position++])
-		{
-		case 'e': dx = 0; break;
-		case 'w': dx = -1; break;
-		default: return false;
-		}
-		break;
-	default: return false;
-	}
-	return true;
-}
 
 int main(int argc, const  char* argv[])
 {
@@ -46,27 +18,47 @@ int main(int argc, const  char* argv[])
 	{
 		int x = 0, y = 0;
 
-		int position = 0;
-		int dx, dy;
-		while (position < (int)line.size() && read_direction(line, position, dx, dy))
-			x += dx, y += dy;
+		auto it = line.begin();
+		while (it != line.end())
+		{
+			switch(*(it++))
+			{
+			case 'e': x++; break;
+			case 'w': x--; break;
+			case 'n': y--;
+				switch (*(it++))
+				{
+				case 'e': x++; break;
+				case 'w': 
+				default: break;
+				}
+				break;
+			case 's': y++;
+				switch (*(it++))
+				{
+				case 'w': x--; break;
+				case 'e':
+				default: break;
+				}
+				break;
+			default:
+				break;
+			}
+		}
 
 		floor_tiles[{x,y}] ^= 1;
 	}
 
 	for (int day = 0; day < 100; day++)
 	{
-
 		std::map<std::pair<int, int>,int> neighbour_counts;
 
-		for (const auto& pair : floor_tiles)
+		for (const auto& [pos, flipped] : floor_tiles)
 		{
-			if (!pair.second)
+			if (!flipped)
 				continue;
 
-			const auto& pos = pair.first;
-
-			neighbour_counts[pos]; // lol
+			neighbour_counts[pos]; // needs to exist
 			neighbour_counts[{pos.first+1, pos.second  }]++; // e
 			neighbour_counts[{pos.first-1, pos.second  }]++; // w
 			neighbour_counts[{pos.first+1, pos.second-1}]++; // ne
@@ -75,29 +67,15 @@ int main(int argc, const  char* argv[])
 			neighbour_counts[{pos.first-1, pos.second+1}]++; // sw
 		}
 
-		for (const auto& pair : neighbour_counts)
+		for (const auto& [pos, count] : neighbour_counts)
 		{
-			bool& tile = floor_tiles[pair.first];
-			
-			if (tile)
-			{
-				if (pair.second == 0 || pair.second > 2)
-					tile = false;
-			}
-			else
-			{
-				if (pair.second == 2)
-					tile = true;
-			}
+			bool& tile = floor_tiles[pos];
+			tile = tile ? (count != 0 && count <= 2) : (count == 2);
 		}
 	}
 
-	int count = 0;
-	for (const auto& pair : floor_tiles)
-		if (pair.second)
-			count++;
-
-	printf("%d", count);
+	std::cout << std::count_if(floor_tiles.begin(), floor_tiles.end(),
+							   [](const auto pair) { return pair.second; });
 
 	return 0;
 }
