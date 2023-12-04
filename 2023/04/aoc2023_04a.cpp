20ef345814decd01be23ea6d469c178fdb40f951
Alasdair <alasdairwjallen@gmail.com>
2023-12-04 19:11:28 +0000

    Moved 2023/04 and fixed part a

---
+++ 2023/04/aoc2023_04a.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <regex>
+#include <ranges>
+#include <charconv>
+#include <set>
+
+#include "../../util/charconv.hpp" // util::from_chars
+
+const std::regex card_pattern{ "Card \\s*\\d+\\: (.*) \\| (.*)" };
+
+int main(int _, const char*[])
+{
+	int total = 0;
+
+	for (std::string line; std::getline(std::cin, line); )
+	{
+		std::smatch match;
+		std::regex_match(line, match, card_pattern);
+
+		const std::string_view winning_numbers_str{ match[1].first, match[1].second };
+		const std::string_view numbers_str{ match[2].first, match[2].second };
+
+		std::set<int> winners;
+		for (const auto winning_number_str : winning_numbers_str | std::views::split(' '))
+		{
+			if (!winning_numbers_str.empty())
+			{
+				if (int number; util::from_chars(winning_number_str, number))
+				{
+					winners.insert(number);
+				}
+			}
+		}
+
+		int match_count = 0;
+		for (const auto number_str : numbers_str | std::views::split(' '))
+		{
+			if (!numbers_str.empty())
+			{
+				if (int number; util::from_chars(number_str, number))
+				{
+					if (winners.contains(number))
+					{
+						match_count++;
+					}
+				}
+			}
+		}
+
+		if (match_count != 0)
+		{
+			total += (1 << (match_count - 1));
+		}
+	}
+
+	std::cout << total;
+}
