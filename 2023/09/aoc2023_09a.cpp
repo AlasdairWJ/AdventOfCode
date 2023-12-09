0efaacd7f17b14494509207b623e138e988b6a85
Alasdair <alasdairwjallen@gmail.com>
2023-12-09 11:16:54 +0000

    Added 2023/09

---
+++ 2023/09/aoc2023_09a.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ranges> // std::views::adjacent
+#include <algorithm> // std::ranges::all_of
+
+#include "../../util/charconv.hpp" // util::from_chars
+
+int main(int _, const char*[])
+{
+	int total = 0;
+
+	for (std::string line; std::getline(std::cin, line); )
+	{
+		std::vector<int> values;
+		
+		for (auto && r : line | std::views::split(' '))
+		{
+			if (int value; util::from_chars(r, value))
+				values.push_back(value);
+		}
+
+		int next = values.back();
+
+		while (!std::ranges::all_of(values, [](int x) { return x == 0; }))
+		{
+			std::vector<int> differences;
+			for (const auto [a, b] : values | std::views::adjacent<2>)
+				differences.push_back(b - a);
+
+			next += differences.back();
+
+			values.swap(differences);
+		}
+
+		total += next;
+	}
+
+	std::cout << total;
+} 
