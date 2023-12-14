e81f8bd7fff612b3a34420aaa18d40ff7492a770
Alasdair <alasdairwjallen@gmail.com>
2023-12-14 20:15:54 +0000

    Improved 2023/01b
    im dumb

--- 2023/01/aoc2023_01b.cpp
+++ 2023/01/aoc2023_01b.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <string> // std::getline
 #include <string_view>
-#include <algorithm>
-#include <ranges>
+#include <algorithm> // std::begin, std::distance, std::ranges::min_element, std::ranges::max_element
 
 constexpr char digits[]{ "0123456789" };
 constexpr std::string_view digit_names[]{
@@ -18,24 +17,16 @@ constexpr std::string_view digit_names[]{
 	"nine"
 };
 
-std::size_t first_index_of_sequence(const std::string_view source, const std::string_view sequence)
+std::size_t first_index_of(const std::string_view source, const std::string_view sequence)
 {
-	const auto result = std::ranges::search(source, sequence);
-	return std::distance(source.begin(), result.begin());
+	const std::size_t ix = source.find(sequence);
+	return ix != std::string::npos ? ix : source.size();
 }
 
-std::size_t last_index_of_sequence(const std::string_view source, const std::string_view sequence)
+std::size_t last_index_of(const std::string_view source, const std::string_view sequence)
 {
-	if (sequence.size() > source.size())
-		return 0;
-
-	for (std::size_t i = source.size() - sequence.size() + 1; i > 0; i--)
-	{
-		if (source.substr(i - 1).starts_with(sequence))
-			return i;
-	}
-
-	return 0;
+	const std::size_t ix = source.rfind(sequence);
+	return ix != std::string::npos ? ix + 1 : 0;
 }
 
 int main(int _, const char*[])
@@ -48,7 +39,7 @@ int main(int _, const char*[])
 		first_indices[0] = line.find_first_of(digits);
 
 		for (int d = 1; d < 10; d++)
-			first_indices[d] = first_index_of_sequence(line, digit_names[d]);
+			first_indices[d] = first_index_of(line, digit_names[d]);
 
 		const std::size_t first_digit_ix = std::distance(std::begin(first_indices), std::ranges::min_element(first_indices));
 
@@ -58,7 +49,7 @@ int main(int _, const char*[])
 		last_indices[0] = line.find_last_of(digits) + 1;
 
 		for (int d = 1; d < 10; d++)
-			last_indices[d] = last_index_of_sequence(line, digit_names[d]);
+			last_indices[d] = last_index_of(line, digit_names[d]);
 
 		const std::size_t last_digit_ix = std::distance(std::begin(last_indices), std::ranges::max_element(last_indices));
 
