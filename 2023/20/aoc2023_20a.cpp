a8730be9969014ba41121f8160afda4015605e0c
Alasdair <alasdairwjallen@gmail.com>
2023-12-23 12:27:46 +0000

    Moved & updated 2023/20a

---
+++ 2023/20/aoc2023_20a.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <string> // std::getline
+#include <string_view>
+#include <vector>
+#include <map>
+#include <ranges> // std::views::split
+#include <algorithm> // std::ranges::all_of
+
+enum class Type
+{
+	None,
+	FlipFlop,
+	Conjunction
+};
+
+constexpr bool Low = false;
+constexpr bool High = true;
+
+constexpr char Broadcaster[]{ "broadcaster" };
+
+const std::string_view delim{ ", " };
+
+struct Module
+{
+	Type type;
+	std::vector<std::string> connections;
+	bool state;
+
+	std::map<std::string, bool> inputHistory;
+};
+
+int main(int _, const char*[])
+{
+	std::map<std::string, Module> modules;
+
+	std::vector<std::string> broadcasterConnections;
+
+	for (std::string line; std::getline(std::cin, line); )
+	{
+		if (line.starts_with(Broadcaster))
+		{
+			const std::size_t ix = line.find(" -> ");
+
+			const auto connections_str = std::string_view{ line }.substr(ix + 4);
+			
+			for (auto && r : connections_str | std::views::split(delim))
+			{
+				broadcasterConnections.emplace_back(r.begin(), r.end());
+			}
+		}
+		else
+		{
+			const std::size_t ix = line.find(" -> ");
+			const auto name = line.substr(1, ix - 1);
+
+			auto& module = modules[name];
+			module.type = (line[0] == '%') ? Type::FlipFlop : Type::Conjunction;
+			module.state = false;
+
+			const auto connections_str = std::string_view{ line }.substr(ix + 4);
+			for (auto && r : connections_str | std::views::split(delim))
+			{
+				module.connections.emplace_back(r.begin(), r.end());
+			}
+		}
+	}
+
+	for (const auto& connection : broadcasterConnections)
+	{
+		if (auto it = modules.find(connection); it != modules.end())
+		{
+			it->second.inputHistory[Broadcaster] = false;
+		}
+	}
+
+	for (const auto& [moduleName, module] : modules)
+	{
+		for (const auto& connection : module.connections)
+		{
+			if (auto it = modules.find(connection); it != modules.end())
+			{
+				it->second.inputHistory[moduleName] = false;
+			}
+		}
+	}
+
+	long long pulses[2]{};
+
+	for (int i = 0; i < 1'000; i++)
+	{
+		std::vector<std::pair<std::string, bool>> pulseBuffer;
+		
+		pulseBuffer.emplace_back(Broadcaster, Low);
+
+		pulses[Low]++;
+
+		while (!pulseBuffer.empty())
+		{
+			decltype(pulseBuffer) nextPulseBuffer;
+
+			for (const auto& [source, state] : pulseBuffer)
+			{
+				const auto& destinations = (source == Broadcaster)
+					? broadcasterConnections
+					: modules.find(source)->second.connections;
+
+				for (const auto& destination : destinations)
+				{
+				//	std::cout << source << " --["
+				//		<< (pulse.state ? "High" : "Low")
+				//		<< "]-> " << destination << '\n';
+
+					auto it = modules.find(destination);
+					if (it == modules.end())
+						continue;
+
+					auto& module = it->second;
+
+					switch (module.type)
+					{
+					case Type::FlipFlop:
+						if (state == Low)
+						{
+							module.state ^= true;
+							nextPulseBuffer.emplace_back(destination, module.state);
+						}
+						break;
+					case Type::Conjunction:
+						module.inputHistory[source] = state;
+						module.state = std::ranges::all_of(module.inputHistory | std::views::values, std::identity{});
+						if (modules.contains(destination))
+							nextPulseBuffer.emplace_back(destination, !module.state);
+						break;
+					default:
+						break;
+					}
+				}
+
+				pulses[state] += destinations.size();
+			}
+
+			pulseBuffer.swap(nextPulseBuffer);
+		}
+	}
+
+//	std::cout << pulses[0] << " * ";
+//	std::cout << pulses[1] << " = ";
+	std::cout << pulses[0] * pulses[1];
+}
