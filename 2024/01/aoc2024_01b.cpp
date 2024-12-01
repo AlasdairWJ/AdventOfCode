8bba4572da16fa78e57cf1719b0d86bb3b5dc5a9
Alasdair <alasdairwjallen@gmail.com>
2024-12-01 10:34:53 +0000

    Added 2024/01
    it's that time again

---
+++ 2024/01/aoc2024_01b.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+int main(int _, const char*[])
+{
+	std::vector<int> left;
+	std::map<int, int> right;
+
+	for (int a, b; std::cin >> a >> b; )
+	{
+		left.push_back(a);
+		right[b]++;
+	}
+
+	int total = 0;
+
+	for (const int a : left)
+	{
+		if (const auto it = right.find(a); it != right.end())
+			total += a * it->second;
+	}
+
+	std::cout << total;
+}
