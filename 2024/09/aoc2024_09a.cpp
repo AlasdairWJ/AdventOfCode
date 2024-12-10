8b6c47357321d21a0899cd5d70cfb44ec31de0ed
Alasdair <alasdairwjallen@gmail.com>
2024-12-10 21:10:17 +0000

    Refactored 2024/09a

--- 2024/09/aoc2024_09a.cpp
+++ 2024/09/aoc2024_09a.cpp
@@ -3,54 +3,86 @@
 #include <vector>
 #include <ranges>
 
+struct Block
+{
+	int id;
+	int count;
+};
+
+int tri(const int n)
+{
+	return n * (n + 1) / 2;
+}
+
 int main(int argc, char const *argv[])
 {
 	std::string line;
 	std::getline(std::cin, line);
 	
-	std::vector<int> blocks;
+	std::vector<Block> blocks;
+	std::vector<int> counts;
 
 	int id = 0;
 	bool gap = false;
+
 	for (const char c : line)
 	{
 		const int count = int{ c - '0' };
 
-		blocks.push_back(Block{
-			gap ? -1 : id,
-			count
-		});
-
 		if (gap)
+		{
+			blocks.push_back(Block{ -1, count });
+		}
+		else
+		{
+			blocks.push_back(Block{ id, count });
+			counts.push_back(count);
 			id++;
+		}
 
 		gap ^= true;
 	}
 
-	int index = 0;
-	int last = blocks.size() - 1;
-
-	while (blocks[index] >= 0)
-		index++;
-
-	while (index < last)
+	for (--id; id > 0; --id)
 	{
-		blocks[index] = blocks[last];
-		blocks[last] = -1;
+		int space_required = counts[id];
 
-		while (blocks[index] >= 0)
-			index++;
+		auto it = blocks.begin();
 
-		do last--;
-		while (blocks[last] < 0);
+		for (; it->id != id; ++it)
+		{
+			if (space_required > 0 && it->id < 0)
+			{
+				if (it->count <= space_required)
+				{
+					it->id = id;
+					space_required -= it->count;
+				}
+				else
+				{
+					it->id = -1;
+					it->count -= space_required;
+					it = blocks.insert(it, Block{ id, space_required }); // insert new block before this one
+					space_required = 0;
+				}
+			}
+		}
+
+		if (space_required == 0)
+			it->id = -1; // it's all been moved, so clear the original block
+		
+		it->count = space_required;
 	}
 
 	long long checksum = 0;
+	long long position = 0;
 
-	for (const auto [index, value] : blocks | std::views::enumerate)
+	for (const auto [id, count] : blocks)
 	{
-		if (value > 0)
-			checksum += value * index;
+		if (id >= 0)
+			checksum += (count * position + tri(count - 1)) * id;
+
+		position += count;
 	}
 
 	std::cout << checksum;
